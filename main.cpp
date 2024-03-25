f4b1a58cc5ef02410b18599cfb42f124bded70be
Cloud <michalcloud2003@gmail.com>
2024-03-25 16:19:49 +0000

    Fixed Menu to display all info properly with a for loop.

--- main.cpp
+++ main.cpp
@@ -27,37 +27,40 @@ string directionString(int directionInt) {
     }
 }
 
-// Protected bug class
+// Bug class
 class Bug {
-protected:
-    char type;
-    int id;
-    pair<int,int> position;
-    Direction direction;
-    int size;
-    bool alive;
-    list<pair<int,int>> path;
+private:
+    char type; // Type of bug
+    int id; // Identification number
+    pair<int,int> position; // Position coordinates
+    Direction direction; // Direction the bug is facing
+    int size; // Size of the bug
+    bool alive; // Flag indicating if the bug is alive
+    list<pair<int,int>> path; // Path taken by the bug
 
 public:
     // Constructor to initialize variables
-    Bug(char type, int id, pair<int,int> position, Direction direction, int size, bool alive, list<pair<int,int>> path) :
-            type(type), id(id), position(position), direction(direction), size(size), alive(alive), path(path) {}
+    Bug(char _type, int _id, pair<int,int> _position, Direction _direction, int _size, bool _alive, list<pair<int,int>> _path) :
+            type(_type), id(_id), position(_position), direction(_direction), size(_size), alive(_alive), path(_path) {}
 
     // Function to display Bug information
     void display() {
-        printf("%-5c %-6d %d, %-11d %-16s %-5d %-5d ",
-               type, id, position.first, position.second, directionString(static_cast<int>(direction)).c_str(), size, alive);
+        // Print bug information
+        printf("%-5c %-6d %d,%-9d %-16s %-5d %-5s ",
+               type, id, position.first, position.second, directionString(static_cast<int>(direction)).c_str(), size, alive ? "true" : "false");
+
+        // Print path taken by the bug
         for (const auto& p : path) {
-            printf("(%d, %d) ", p.first, p.second);
+            printf("(%d,%d) ", p.first, p.second);
         }
-        printf("\n\n");
+        printf("\n");
     }
 };
 
-
 int main() {
+    vector<Bug> bugs; // Vector to hold all bugs
 
-    int userChoice;
+    int userChoice; // Variable to store user's choice
 
     // Display menu
     cout << "----Menu----" << endl;
@@ -70,26 +73,26 @@ int main() {
     cout << "6. Display all Cells" << endl;
     cout << "7. PLAY GAME" << endl;
     cout << "8. Exit" << endl;
-    cin >> userChoice;
+    cin >> userChoice; // Get user's choice
 
-    ifstream file("Bugs.txt");
+    ifstream file("Bugs.txt"); // Open file for reading bugs
 
-    if (!file.is_open()) {
+    if (!file.is_open()) { // Check if file is successfully opened
         cout << "Cannot Open File!" << endl;
         return 1;
     }
 
-    string line;
-    while (getline(file, line)) {
-        stringstream s(line);
-        char type;
-        int id, x, y, directionInt, size;
-        list<pair<int, int>> path;
+    string line; // Variable to store each line read from file
+    while (getline(file, line)) { // Read each line from file
+        stringstream s(line); // Create string stream from line
+        char type; // Type of bug
+        int id, x, y, directionInt, size; // Bug attributes
+        list<pair<int, int>> path; // Path taken by the bug
 
-        s >> type >> id >> x >> y >> directionInt >> size;
+        s >> type >> id >> x >> y >> directionInt >> size; // Read bug attributes
 
         int tempX, tempY;
-        while (s >> tempX >> tempY) {
+        while (s >> tempX >> tempY) { // Read path taken by the bug
             path.push_back({tempX, tempY});
         }
 
@@ -112,28 +115,50 @@ int main() {
                 cerr << "Invalid direction!" << endl;
                 continue; // Skip this line
         }
-        // Create Bug object and display information
-        Bug bug(type, id, {x, y}, dir, size, true, path);
-        bug.display();
 
-        switch (userChoice) {
-            case 1:
-                break;
-            case 2:
+        // Create Bug object and add it to the vector
+        bugs.push_back(Bug(type, id, {x, y}, dir, size, true, path));
+    }
+
+    // Close the file
+    file.close();
+
+    // Based on user's choice, perform appropriate action
+    switch (userChoice) {
+        case 1:
+            // Initialize Bug Board
+            break;
+        case 2:
+            //Headings
+            printf("%-5s %-6s %s %12s %10s %7s\n",
+                   "Type", "ID", "Position", "Direction", "Size", "Alive");
+            // Display All Bugs
+            for (Bug bug : bugs) {
                 bug.display();
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                cerr << "Invalid Option!" << endl;
-               continue; // Skip this line
+            }
+            break;
+        case 3:
+            // Find Bug By ID
+            break;
+        case 4:
+            // SHAKE BOARD!!!
+            break;
+        case 5:
+            // Display Life History of all bugs
+            break;
+        case 6:
+            // Display all Cells
+            break;
+        case 7:
+            // PLAY GAME
+            break;
+        case 8:
+            // Exit
+            return 0;
+        default:
+            cerr << "Invalid Option!" << endl;
+            break;
     }
 
-       }
-        // Close the file
-        file.close();
-
-        return 0;
-    }
+    return 0;
+}

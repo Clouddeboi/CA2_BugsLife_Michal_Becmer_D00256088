664a2e4ef291cc619e64d9d6330a6c178d88ef35
Cloud <michalcloud2003@gmail.com>
2024-04-25 18:57:37 +0100

    implemented new feature for battle ant, wrote out explanation for how new bug works inside its cpp file.

--- BattleAnt.cpp
+++ BattleAnt.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+/*  BATTLE BUG BEHAVIOUR
+ *
+ *  The battle ants are very large, However,
+ *  since they are so large it takes so much energy to
+ *  move that they get smaller with every turn!
+ *
+ *  FEATURES OF BATTLE ANT
+ *
+ *  -size is a minimum of 15 when adding
+ *  -They cannot get under a size of 3
+ *  -If they eat another bug they grow +1 in size
+ *
+ * */
+
 BattleAnt ::BattleAnt(char type, int id, int x, int y, int direction, int size) : Bug(type, id, x , y , direction, size){}
 
 void BattleAnt ::move() {
@@ -38,9 +52,24 @@ void BattleAnt ::move() {
                 cout << "Moving West" << endl;
                 break;
         }
-        //update new position and print
+        //update new position and set size of bug
         setPosition(newPos);
+        setSize(getSize());
+
+        //implement getting smaller logic
+        if(getSize() > 3)
+        {
+            //if larger than 3, lose _1 size
+            setSize(getSize() - 1);
+        }
+        else if(getSize() < 3)
+        {
+            //if lower than 3, gain +1 size
+            setSize(getSize() + 1);
+        }
+
         cout << "New Position: (" << getPosition().first << ", " << getPosition().second << ")\n" << endl;
+        cout << "Size Decreased!\nCurrent Size: " << getSize() << "\n" << endl;
     }
     else//if the way is blocked
     {
@@ -49,5 +78,5 @@ void BattleAnt ::move() {
         cout<<"This Direction is blocked! Redirecting!\n" << endl;
         move(); //recursive function calling move to move after changing direction
     }
-    //FURTHER IMPLEMENTATION IS NEEDED ONE EAT/FIGHT FUNCTIONALITY IS IMPLEMENTED
+    //FURTHER IMPLEMENTATION IS NEEDED ONCE EAT/FIGHT FUNCTIONALITY IS IMPLEMENTED
 }
